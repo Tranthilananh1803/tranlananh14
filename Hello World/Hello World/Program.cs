bda79c6f0db8a8f44fd8e66cc62370fb3b02499d
Tranthilananh1803 <tranlananhquyet2318@gmail.com>
2021-01-15 15:01:43 +0700

    add new project

---
+++ Hello World/Hello World/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstname = "Anh";
+            string full = "Chao mung"  + firstname + "den voi mon hoc LTQL";
+            Console.WriteLine(full);
+        }
+    }
+}
